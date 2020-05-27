e19c03b569f41933e6d174ffe10c46c698b8d322
nikkesan <nikke.san@gmail.com>
2020-05-27 21:41:49 +0100

    import images to MainPage

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { ReactComponent as Logo_Sync } from "../Assets/Logo_Sync_(Black).svg";
+import { ReactComponent as Logo_X } from "../Assets/Logo_X_(Fluorescent).svg";
+import Laptop from "../Assets/laptop.png";
 
 export const MainPage = () => {
   return (
     <>
+      <Logo_Sync />
+      <img src={Laptop} />
       <h1>Online Login</h1>
       <h2>Enter your mobile number to log into your account</h2>
       <form action="#">
@@ -25,6 +30,7 @@ export const MainPage = () => {
       <p>
         Haven't got an account yet?<button>Sign up</button>
       </p>
+      <Logo_X />
     </>
   );
 };
