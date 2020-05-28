0e62713e7309a3e920aa731fd944440202c65dac
nikkesan <nikke.san@gmail.com>
2020-05-28 09:26:40 +0100

    add background and sync logos to main page

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,36 +1,16 @@
 import React from "react";
-import { ReactComponent as Logo_Sync } from "../Assets/Logo_Sync_(Black).svg";
-import { ReactComponent as Logo_X } from "../Assets/Logo_X_(Fluorescent).svg";
-import Laptop from "../Assets/laptop.png";
+import Img_Laptop from "../Assets/laptop.png";
+import Logo_Sync from "../Assets/Logo_Sync_(White).svg";
+import Logo_X from "../Assets/Logo_X_(Fluorescent).svg";
+import { MainText } from "./MainText/MainText";
 
 export const MainPage = () => {
   return (
     <>
-      <Logo_Sync />
-      <img src={Laptop} />
-      <h1>Online Login</h1>
-      <h2>Enter your mobile number to log into your account</h2>
-      <form action="#">
-        <label for="personal">Personal</label>
-        <input
-          type="radio"
-          id="personal"
-          name="account-type"
-          value="personal"
-        ></input>
-        <label for="business">Business</label>
-        <input
-          type="radio"
-          id="business"
-          name="account-type"
-          value="business"
-        ></input>
-        <button>Continue</button>
-      </form>
-      <p>
-        Haven't got an account yet?<button>Sign up</button>
-      </p>
-      <Logo_X />
+      <img alt="Sync logo" className="logo-sync" src={Logo_Sync}></img>
+      <img alt="laptop" className="img-laptop" src={Img_Laptop} />
+      <MainText />
+      <img alt="X logo" className="logo-x" src={Logo_X}></img>
     </>
   );
 };
